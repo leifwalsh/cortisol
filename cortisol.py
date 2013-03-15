5ac237de33c4d019cf2b6733953df8f70184113c
Leif Walsh <leif.walsh@gmail.com>
2013-03-15 19:55:53 -0400

    add support for config file

--- cortisol.py
+++ cortisol.py
@@ -7,37 +7,26 @@ import logging
 import multiprocessing
 import pymongo
 import random
+import re
+import shlex
 import time
 
-minval=-1000000
-maxval=1000000
+MINVAL = -1000000
+MAXVAL = 1000000
 
-# config
-c = {
-    'collections': 4,
-    'indexes': 4,
-    'fields': 2,
-    'documents': 2**20,
-    'seconds': 600,
-    'ptquery': {
-        'threads': 4,
-        'batch': 50
-        },
-    'scan': {
-        'threads': 2,
-        },
-    'update': {
-        'threads': 4,
-        'batch': 50
-        },
-    'drop': {
-        'threads': 1,
-        'period': 60
-        }
-    }
+class Config(argparse.Namespace):
+    pass
+
+conf = Config(
+    collections = 4,
+    indexes = 4,
+    fields = 2,
+    documents = 2**20,
+    seconds = 600
+    )
 
 fields = 'abcdefghijklmnopqrstuvwxyz'
-assert c['fields'] <= len(fields), 'too many fields'
+assert conf.fields <= len(fields), 'too many fields'
 
 def index_key(i):
     '''Generate a key by combining fields and directions based on the bit pattern of i.'''
@@ -52,7 +41,7 @@ def index_key(i):
             d = pymongo.ASCENDING
         else:
             d = pymongo.DESCENDING
-        assert x < c['fields'], 'too many indexes'
+        assert x < conf.fields, 'too many indexes'
         r.append((fields[x], d))
         x += 1
         i /= 2
@@ -90,10 +79,12 @@ class CollectionThread(multiprocessing.Process):
             raise
         logging.debug('Stopping %s', self.name)
 
+
 class FillThread(CollectionThread):
     '''Fills a collection.'''
     def _run(self):
         self.coll.fill()
+
 
 class StressThread(CollectionThread):
     '''A CollectionThread that runs a repeated operation until a timer expires.'''
@@ -102,12 +93,6 @@ class StressThread(CollectionThread):
         self.stop_event = stop_event
         self.queue = queue
         self.cnt = collections.Counter()
-
-    def __getattr__(self, attr):
-        try:
-            return c[self.key][attr]
-        except KeyError:
-            raise AttributeError
 
     def sleep(self, secs):
         t0 = time.time()
@@ -125,34 +110,41 @@ class StressThread(CollectionThread):
             self.queue.put(self.cnt)
 
 def sampleids(n):
-    return random.sample(xrange(c['documents']), n)
+    return random.sample(xrange(conf.documents), n)
 
 class UpdateThread(StressThread):
-    key = 'update'
+    config = ['threads', 'batch']
+    threads = 4
+    batch = 50
     def step(self):
         # TODO: Construct some invariant-preserving update statement.
         d = {}
-        for f in fields[:c['fields']]:
-            d[f] = random.randint(minval, maxval)
+        for f in fields[:conf.fields]:
+            d[f] = random.randint(MINVAL, MAXVAL)
         self.coll.update({'_id': {'$in': sampleids(self.batch)}}, {'$inc': d}, multi=True)
         self.performance_inc('updates', self.batch)
 
 class ScanThread(StressThread):
-    key = 'scan'
+    config = ['threads']
+    threads = 2
     def step(self):
         # Without the sum, might not force the cursor to iterate over everything.
         suma = sum(item['a'] for item in self.coll.find())
         self.performance_inc('scans')
 
 class PointQueryThread(StressThread):
-    key = 'ptquery'
+    config = ['threads', 'batch']
+    threads = 4
+    batch = 50
     def step(self):
         # Without the sum, might not force the cursor to iterate over everything.
         suma = sum(item['a'] for item in self.coll.find({'_id': {'$in': sampleids(self.batch)}}))
         self.performance_inc('ptqueries', self.batch)
 
 class DropThread(StressThread):
-    key = 'drop'
+    config = ['threads', 'period']
+    threads = 1
+    period = 60
     def step(self):
         self.sleep(self.period)
         if not self.stop_event.is_set():
@@ -173,7 +165,7 @@ class Collection(pymongo.collection.Collection):
         return '%s.%s' % (self.database.name, self.name)
 
     def ensure_indexes(self):
-        for i in range(c['indexes']):
+        for i in range(conf.indexes):
             k = index_key(i)
             logging.debug('creating index %s', k)
             self.create_index(k)
@@ -182,21 +174,21 @@ class Collection(pymongo.collection.Collection):
         logging.info('Filling %s', self.fullname)
         def zero_doc(i):
             d = {'_id': i}
-            for f in fields[:c['fields']]:
+            for f in fields[:conf.fields]:
                 d[f] = 0
             return d
 
-        docs = (zero_doc(i) for i in xrange(c['documents']))
+        docs = (zero_doc(i) for i in xrange(conf.documents))
 
         inserted = 0
         for chunk in chunks(docs, 10000):
             self.insert(chunk, manipulate=False)
-            inserted = min(inserted + 10000, c['documents'])
-            logging.debug('Filling %s %d/%d', self.fullname, inserted, c['documents'])
+            inserted = min(inserted + 10000, conf.documents)
+            logging.debug('Filling %s %d/%d', self.fullname, inserted, conf.documents)
 
 def setup(db):
     colls = []
-    for i in range(c['collections']):
+    for i in range(conf.collections):
         colls.append(Collection(db, i))
     return colls
 
@@ -220,12 +212,12 @@ def stress(colls):
     try:
         logging.info('Starting stress test')
         for coll in colls:
-            for klass in [UpdateThread, PointQueryThread, ScanThread, DropThread]:
-                for _ in range(c[klass.key]['threads']):
-                    t = klass(coll, stop_event, queue)
+            for cls in StressThread.__subclasses__():
+                for _ in range(cls.threads):
+                    t = cls(coll, stop_event, queue)
                     threads.append(t)
                     t.start()
-        time.sleep(c['seconds'])
+        time.sleep(conf.seconds)
         logging.info('Stopping stress test')
     except KeyboardInterrupt:
         pass
@@ -244,6 +236,7 @@ def stress(colls):
 def main():
     parser = argparse.ArgumentParser(description='Stress test for mongodb.')
     parser.add_argument('--verbose', '-v', action='count')
+    parser.add_argument('--config', '-c', type=str)
 
     conn_args = parser.add_argument_group('Connection')
     conn_args.add_argument('--host', default='127.0.0.1', type=str)
@@ -255,28 +248,39 @@ def main():
     stress_or_create.add_argument('--only-create', action='store_true')
     exec_args.add_argument('--keep-database', action='store_true')
 
-    def add_conf_item(arg_group, d, key, val, prefix=None):
-        if isinstance(val, collections.MutableMapping):
-            groupname = '%s Thread Configuration' % key.capitalize()
-            nested_group = parser.add_argument_group(groupname)
-            for nk, nv in val.items():
-                add_conf_item(nested_group, val, nk, nv, '%s-' % key)
+    def add_conf_item(arg_group, obj, key, default_value, prefix=None):
+        if prefix is None:
+            flag = '%s' % key
         else:
-            if prefix is None:
-                flag = '%s' % key
-            else:
-                flag = '%s%s' % (prefix, key)
-            def setter_call(self, parser, namespace, values, option_string=None):
-                self.the_dest[self.the_destkey] = values
-            setter_class = type('%s-setter' % flag, (argparse.Action,),
-                                {'the_dest': d, 'the_destkey': key, '__call__': setter_call})
-            arg_group.add_argument('--%s' % flag, default=val, type=type(val), action=setter_class)
+            flag = '%s-%s' % (prefix, key)
+        def setter_call(self, parser, namespace, values, option_string=None):
+            setattr(self.obj, self.key, values)
+        setter_class = type('%s-setter' % flag, (argparse.Action,),
+                            {'obj': obj, 'key': key, '__call__': setter_call})
+        arg_group.add_argument('--%s' % flag, default=default_value,
+                               type=type(default_value), action=setter_class)
 
     conf_args = parser.add_argument_group('Configuration')
-    for topkey, topval in c.items():
-        add_conf_item(conf_args, c, topkey, topval)
+    for k, v in conf.__dict__.items():
+        add_conf_item(conf_args, conf, k, v)
+
+    for cls in StressThread.__subclasses__():
+        match = re.match('([a-zA-Z0-9]+)Thread', cls.__name__)
+        capitalized = match.group(1)
+        lowercase = capitalized.lower()
+        cls_args = parser.add_argument_group('%s Thread Configuration' % capitalized)
+        for k in cls.config:
+            v = getattr(cls, k)
+            add_conf_item(cls_args, cls, k, v, lowercase)
 
     args = parser.parse_args()
+
+    if args.config is not None:
+        with open(args.config, 'r') as config:
+            configdata = config.read()
+        args = parser.parse_args(shlex.split(configdata, '#'))
+        # re-parse sys.argv to override things in config
+        args = parser.parse_args(namespace=args)
 
     if args.verbose is None:
         args.verbose = 0

8fe5724175403bfb4b6e7a89d4799cf841a3b878
Leif Walsh <leif.walsh@gmail.com>
2013-08-26 11:59:56 -0400

    add better config file support, can specify multiple files on cli
    syntax is ./cortisol @file1.cnf --cli-opts=here [@file2.cnf [@file3.cnf...]]

--- options.cpp
+++ options.cpp
@@ -1,7 +1,9 @@
 /* -*- mode: C++; c-file-style: "Google"; c-basic-offset: 4 -*- */
 
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <utility>
 
 #include <boost/program_options.hpp>
 
@@ -13,6 +15,7 @@ namespace cortisol {
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::ifstream;
 using std::string;
 
 namespace po = boost::program_options;
@@ -111,22 +114,48 @@ po::options_description Options::options_description() const {
 }
 
 bool parse_cmdline(int argc, const char *argv[], Options &opts) {
-    po::options_description all_options("Options");
-    all_options.add_options()
+    po::options_description visible_options("Options");
+    visible_options.add_options()
             ("help,h", "Get help.")
             ("verbose,v", "More verbose output.")
             ;
 
-    all_options.add(opts.options_description());
+    visible_options.add(opts.options_description());
+
+    po::options_description all_options;
+    all_options.add_options()
+            ("response-file", po::value<vector<string> >(), "Config file.")
+            ;
+
+    all_options.add(visible_options);
 
     try {
+        po::variables_map cli_vm;
+        auto cli_parsed = po::parse_command_line(argc, argv, all_options, 0,
+                                                 [](const string &s) -> std::pair<string, string> {
+                                                     if (s[0] == '@') {
+                                                         return std::make_pair(string("response-file"), s.substr(1));
+                                                     } else {
+                                                         return std::pair<string, string>();
+                                                     }
+                                                 });
+        po::store(cli_parsed, cli_vm);
+
         po::variables_map vm;
-        po::store(po::parse_config_file<char>("cortisol.cnf", all_options), vm);
-        po::store(po::parse_command_line(argc, argv, all_options), vm);
+        if (cli_vm.count("response-file")) {
+            const vector<string> files = cli_vm["response-file"].as<vector<string> >();
+            for (vector<string>::const_iterator it = files.begin(); it != files.end(); ++it) {
+                ifstream ifs(it->c_str());
+                if (ifs.good()) {
+                    po::store(po::parse_config_file(ifs, all_options), vm);
+                }
+            }
+        }
+        po::store(cli_parsed, vm);
 
         if (vm.count("help")) {
             cout << "cortisol:" << endl
-                 << all_options << endl;
+                 << visible_options << endl;
             return false;
         }
 
@@ -138,7 +167,7 @@ bool parse_cmdline(int argc, const char *argv[], Options &opts) {
         return true;
     } catch(po::error &e) {
         cerr << "Error parsing command line: " << e.what() << endl << endl
-             << all_options << endl;
+             << visible_options << endl;
         return false;
     }
 }
